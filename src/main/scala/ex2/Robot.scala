84030da90421312afda965fc89f102135db63a9c
thomastesta <thomas.testa@studio.unibo.it>
2026-05-04 11:46:37 +0200

    Exercise 2

--- src/main/scala/ex2/Robot.scala
+++ src/main/scala/ex2/Robot.scala
@@ -1,5 +1,7 @@
 package ex2
 
+import scala.util.Random
+
 type Position = (Int, Int)
 enum Direction:
   case North, East, South, West
@@ -42,9 +44,69 @@ class LoggingRobot(val robot: Robot) extends Robot:
     robot.act()
     println(robot.toString)
 
+
+class RobotWithBattery(val robot: Robot, var battery: Int, val cost: Int) extends Robot:
+  export robot.{position, direction, turn}
+
+  override def act(): Unit =
+    if battery >= cost then
+      robot.act()
+      battery -= cost
+    else
+      println("Battery empty!")
+
+  override def toString: String = s"${robot.toString} (battery=$battery)"
+  
+  
+class RobotCanFail(val robot: Robot, val failProb: Double) extends Robot:
+    export robot.{position, direction, turn}
+
+    override def act(): Unit =
+      if Random.nextDouble() >= failProb then
+        robot.act()
+      else
+        println("Action failed!")
+
+    override def toString: String = s"${robot.toString} (can fail)"
+  
+class RobotRepeated(val robot: Robot, val times: Int) extends Robot:
+    export robot.{position, direction, turn}
+
+    override def act(): Unit =
+      for _ <- 1 to times do
+        robot.act()
+
+    override def toString: String = s"${robot.toString} (repeated $times times)"
+
+
+
+@main def testRobot(): Unit =
+  import ex2.*
+  
+  val robot =
+    LoggingRobot(
+      RobotRepeated(
+        RobotWithBattery(
+          RobotCanFail(
+            SimpleRobot((0, 0), Direction.North),
+            0.2
+          ),
+          battery = 5,
+          cost = 1
+        ),
+        times = 2
+      )
+    )
+
+  robot.act()
+  robot.turn(robot.direction.turnRight)
+  robot.act()
+  robot.act()
+/*
 @main def testRobot(): Unit =
   val robot = LoggingRobot(SimpleRobot((0, 0), Direction.North))
   robot.act() // robot at (0, 1) facing North
   robot.turn(robot.direction.turnRight) // robot at (0, 1) facing East
   robot.act() // robot at (1, 1) facing East
   robot.act() // robot at (2, 1) facing East
+*/
