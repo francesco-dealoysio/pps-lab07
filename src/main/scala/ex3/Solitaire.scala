e857c304c9b9fe2d000c75d674e15996e23da13d
Francesco de Aloysio <francesco.dealoysio@studio.unibo.it>
2026-05-06 12:21:59 +0200

    To be revised

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,18 +1,15 @@
 package ex3
 
   object Solitaire extends App:
-
     type Pos = (Int, Int)
     type Solution = Seq[Pos]
     type SolutionFactory = Solution => Iterable[Solution]
-
-    given SolutionFactory = LazyList(_)
-
-    val width = 6
-    val height = 6
+    val width = 5
+    val height = 5
     val total = width * height
     val start: Pos = (width / 2, height / 2)
-    val MAX_PRINT_SOLUTIONS = 10
+    val MAX_PRINT_SOLUTIONS = Int.MaxValue //10
+    given SolutionFactory = LazyList(_)
 
     def render(solution: Seq[Pos]): String =
       val reversed = solution.reverse
@@ -28,17 +25,16 @@ package ex3
       rows.mkString("\n")
 
     def placeMarks(n: Int = total)(using factory: SolutionFactory): Iterable[Solution] = n match
-      case 1 =>
-        factory(Seq(start))
-
+      case 1 =>  factory(Seq(start))
       case _ =>
         for
           path <- placeMarks(n - 1)
-          next <- nextPositions(path.head)
-          if isSafe(next, path)
+          next <- nextPositions(path) // y
+          //next <- nextPositions(path.head)
+          //if isSafe(next, path)
         yield
-          next +: path.toSeq
-
+          next +: path
+/*
       def isInside(pos: Pos): Boolean =
         val (x, y) = pos
         x >= 0 && x < width && y >= 0 && y < height
@@ -58,6 +54,24 @@ package ex3
 
       def isSafe(pos: Pos, path: Iterable[Pos]): Boolean =
         !path.exists(_ == pos)
+*/
+      // y
+      def nextPositions(path: Iterable[Pos]): Iterable[Pos] =
+        val (x, y) = path.head
+        List(
+          (x + 3, y),
+          (x - 3, y),
+          (x, y + 3),
+          (x, y - 3),
+          (x + 2, y + 2),
+          (x + 2, y - 2),
+          (x - 2, y + 2),
+          (x - 2, y - 2)
+        ).filter(isAcceptable(_, path))
+
+      // y
+      def isAcceptable(pos: Pos, path: Iterable[Pos]): Boolean =
+        pos._1 >= 0 && pos._1 < width && pos._2 >= 0 && pos._2 < height && !path.exists(_ == pos)
 
     val solutions = placeMarks()
 
